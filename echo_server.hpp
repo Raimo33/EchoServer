e52f768916f917336ddd5965353b4f038136ff82
Claudio Raimondi <claudio.raimondi@protonmail.com>
2024-05-01 23:06:51 +0200

    First commit

---
+++ echo_server.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo_server.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 17:43:43 by craimond          #+#    #+#             */
+/*   Updated: 2024/05/01 23:06:22 by craimond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ECHO_SERVER_HPP
+# define ECHO_SERVER_HPP
+
+# include <string>
+# include <iostream>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <unistd.h>
+# include <cstring>
+# include <cstdio>
+# include <cstdlib>
+
+using namespace std;
+
+# define BUFFER_SIZE 1024
+
+#endif
