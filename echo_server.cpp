6ae924f3dbaa8fa3b19a04be4f472197e34041ec
Claudio Raimondi <craimond@c1r3p4.42firenze.it>
2024-05-12 15:18:18 +0200

    Add exceptions

--- echo_server.cpp
+++ echo_server.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   echo_server.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 17:00:28 by craimond          #+#    #+#             */
-/*   Updated: 2024/05/01 22:58:02 by craimond         ###   ########.fr       */
+/*   Updated: 2024/05/12 15:18:14 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,25 @@ int main(int argc, char **argv)
 	int server_socket;
 	int client_socket;
 
-	check_args(argc, argv);
-	server_socket = setup_server(argv);
-	client_socket = connect_with_client(server_socket);	
-	listen_and_echo(client_socket);
-	clean_exit(server_socket, client_socket);
+	try
+	{
+		check_args(argc, argv);
+		server_socket = setup_server(argv);
+		client_socket = connect_with_client(server_socket);	
+		listen_and_echo(client_socket);
+		clean_exit(server_socket, client_socket);
+	}
+	catch (exception &e)
+	{
+		cerr << e.what() << endl;
+	}
 }
 
 void	check_args(int argc, char **argv)
 {
 	if (argc != 2)
-	{
-		cerr << "Usage: " << argv[0] << " <port>" << endl;
-		exit(EXIT_FAILURE);
-	}
+		throw WrongArgsException();
+	(void)argv;
 }
 
 int	setup_server(char **argv)
@@ -52,15 +57,9 @@ int	setup_server(char **argv)
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(port); //cambia la endianess della porta per essere uguale a quella del server
 	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
+		throw BindFailedException();
 	if (listen(server_socket, 3) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
+		throw ListenFailedException();
 	return (server_socket);
 }
 
@@ -92,11 +91,13 @@ void	listen_and_echo(int client_socket)
 		bytes_received = recv(client_socket, buffer, BUFFER_SIZE - 1, 0); //read
 		if (bytes_received <= 0)
 			break;
+		buffer[bytes_received - 1] = '\0';
 		cout << "Received: '" << buffer << "', echoing back..." << endl;
 		if (send(client_socket, buffer, bytes_received, 0) < 0) //write
 			break;
 	}
-	perror("Exiting");
+	if (errno)
+		perror("Exiting");
 }
 
 void	clean_exit(int server_socket, int client_socket)
--- echo_server.hpp
+++ echo_server.hpp
@@ -6,7 +6,7 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 17:43:43 by craimond          #+#    #+#             */
-/*   Updated: 2024/05/01 23:06:22 by craimond         ###   ########.fr       */
+/*   Updated: 2024/05/12 15:17:41 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,36 @@
 # include <cstring>
 # include <cstdio>
 # include <cstdlib>
+# include <exception>
+# include <errno.h>
 
 using namespace std;
 
 # define BUFFER_SIZE 1024
+
+
+class WrongArgsException : public exception
+{
+	virtual const char* what() const throw()
+	{
+		return "Usage: ./echo_server <port>";
+	}
+};
+
+class BindFailedException : public exception
+{
+	virtual const char* what() const throw()
+	{
+		return "Bind failed";
+	}
+};
+
+class ListenFailedException : public exception
+{
+	virtual const char* what() const throw()
+	{
+		return "Listen failed";
+	}
+};
 
 #endif
